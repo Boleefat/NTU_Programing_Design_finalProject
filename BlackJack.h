ad67e20dc30542a4fbd34fad31752abd1248bab0
Kaiwei Chen <127728415+kaiwei1016@users.noreply.github.com>
2023-12-12 19:50:56 +0800

    Add files via upload

---
+++ BlackJack.h
@@ -0,0 +1,46 @@
+// structs /////////////////////
+struct Card;
+struct SkillCounter;
+
+
+
+// classes /////////////////////
+class CardDeck;
+class Record;
+class Game;
+class Player;
+
+// Characters（繼承Player)
+class Seeker;
+class Targetor;
+
+// 還沒寫到的
+class Enemy;
+class Item;
+
+
+
+
+// Card //////////////////////////////////////////
+struct Card
+{
+    string suit;
+    string rank;
+    int value;
+    bool isInDeck;
+
+    // 建構函式
+    Card(const string &s, const string &r, int v);
+};
+
+
+// SkillCounter /////////////////////////////////////
+struct SkillCounter
+{
+    bool isSkillOneAvailable;
+    bool isSkillTwoAvailable;
+    bool isSkillThreeAvailable;
+
+    // 建構函式
+    SkillCounter();
+};
