3118a77189d971a29dbf35df1b06b5c7413e53fe
Kaiwei Chen <127728415+kaiwei1016@users.noreply.github.com>
2023-12-13 11:14:38 +0800

    更新註解

--- test/BlackJack.h
+++ test/BlackJack.h
@@ -8,22 +8,29 @@
 using namespace std;
 
 // structs ------------------------
+
 struct Card;
 struct SkillCounter;
 
 // classes ------------------------
-class CardDeck;
-class Game;
-class Player;
-class Record;
-class Item;  // 道具卡 還沒寫
+
+class Player;  // 必須是一個純粹抽象類別（pure abstract class）
 
 // 兩種Characters, 繼承自Player
 class Seeker;
 class Targetor;
 
-// 莊家, 繼承自Player
+// 由電腦操作的莊家, 繼承自Player
 class Enemy;
+
+class CardDeck;
+class Game;
+
+
+class Item;  // 道具卡 還沒寫
+class Record;  // 玩家的記錄檔
+
+
 
 
 
@@ -50,6 +57,98 @@ struct SkillCounter
 
     // 建構函式
     SkillCounter();
+};
+
+
+
+
+/* Player class, 是一個pure abstract class -----------------------------
+ 是遊戲中所有玩家（包含敵我）的基礎類別，裡面定義了所有玩家都需要有的屬性和行為 */
+
+class Player
+{
+protected:
+    string name;
+    vector<Card> hand;
+    SkillCounter skillCounter;
+    Record* record;
+public:
+    virtual ~Player() = default; // 虛擬析構函數
+    Player(const string &playerName);
+
+    bool operator<(const Player &other) const;
+
+    bool operator>(const Player &other) const;
+
+    int calculateHandValue() const;
+
+    void showHand() const;
+
+    void randomlyAddOneCard(CardDeck &deck);
+
+    // 在 Player 類別中新增函式
+    void addSpecificCard(Card *card);
+
+    string getName() const;
+
+    vector<Card> &getHand();
+};
+
+
+
+
+
+/* Seeker class, 繼承 Player 這個類別 ---------------------------
+是玩遊戲的人可以選擇的我方角色，定義所有Seeker的共同屬性 */
+
+class Seeker : public Player
+{
+public:
+    virtual ~Seeker() = default; // 虛擬析構函數
+
+    // Constructor for Seeker
+    Seeker(const string &playerName);
+
+    // skillOne: Seeker uses a special ability to seek another player's hand
+    void seekAnotherPlayer(Player *targetPlayer);
+
+    // skillTwo: Seeker uses a special ability to seek the deck
+    void seekDeck(CardDeck &deck);
+};
+
+
+
+
+/* Targetor class, 繼承 Player 這個類別 ----------------------
+是玩遊戲的人可以選擇的我方角色，定義所有Targetor的共同屬性 */
+
+class Targetor : public Player
+{
+public:
+    virtual ~Targetor() = default; // 虛擬析構函數
+
+    // Constructor for Targetor
+    Targetor(const string &playerName);
+
+    // skillOne
+    void takeSpecificCard(CardDeck &deck, const string &suit, const string &rank);
+
+    // skillTwo
+    void discardCard(Player *targetPlayer, int value);
+};
+
+
+
+
+
+/* Enemy class, 繼承 Player 這個類別 --------------------------
+ 是由電腦自動操作的敵方角色，定義所有Enemy的共同屬性 */
+
+class Enemy : public Player
+{
+public:
+    // Constructor for Enemy
+    Enemy(const string &enemyName);
 };
 
 
@@ -88,110 +187,9 @@ public:
 
 
 
-// Record class -------------------------------------
-class Record
-{
-private:
-    string playerName;  // 玩家名稱
-    string filePath;    // 檔案路徑
-    int maxCards;       // 單局最高手牌數
-    int maxPoints;      // 單局最高點數
-    int maxWinStreak;   // 最高連勝局數
-public:
-    Record(string playerName);  // 建構函式（輸入玩家名稱）
-    void updateRecord(int cardCnt, int pointCnt, int winStreak);  // 更新玩家檔案
-    void print() const;  // 列印玩家檔案
-};
+/* Game class -------------------------------------------
+隨著遊戲進行，會陸續新增敵人、物品，玩家也會跟這些敵人和物品互動 */
 
-
-
-
-
-// Player class ------------------------------------------
-class Player
-{
-protected:
-    string name;
-    vector<Card> hand;
-    SkillCounter skillCounter;
-    Record* record;
-public:
-    virtual ~Player() = default; // 虛擬析構函數
-    Player(const string &playerName);
-
-    bool operator<(const Player &other) const;
-
-    bool operator>(const Player &other) const;
-
-    int calculateHandValue() const;
-
-    void showHand() const;
-
-    void randomlyAddOneCard(CardDeck &deck);
-
-    // 在 Player 類別中新增函式
-    void addSpecificCard(Card *card);
-
-    string getName() const;
-
-    vector<Card> &getHand();
-};
-
-
-
-
-
-// Enemy class, 繼承自Player---------------------------------
-class Enemy : public Player
-{
-public:
-    // Constructor for Enemy
-    Enemy(const string &enemyName);
-};
-
-
-
-
-// Seeker class, 繼承自Player -------------------------------
-class Seeker : public Player
-{
-public:
-    virtual ~Seeker() = default; // 虛擬析構函數
-
-    // Constructor for Seeker
-    Seeker(const string &playerName);
-
-    // skillOne: Seeker uses a special ability to seek another player's hand
-    void seekAnotherPlayer(Player *targetPlayer);
-
-    // skillTwo: Seeker uses a special ability to seek the deck
-    void seekDeck(CardDeck &deck);
-};
-
-
-
-
-// Targetor class, 繼承自Player ---------------------------------
-class Targetor : public Player
-{
-public:
-    virtual ~Targetor() = default; // 虛擬析構函數
-
-    // Constructor for Targetor
-    Targetor(const string &playerName);
-
-    // skillOne
-    void takeSpecificCard(CardDeck &deck, const string &suit, const string &rank);
-
-    // skillTwo
-    void discardCard(Player *targetPlayer, int value);
-};
-
-
-
-
-
-// Game class ------------------------------------------------
 class Game
 {
 private:
@@ -237,4 +235,23 @@ public:
 
     // destructor
     ~Game();
+};
+
+
+
+
+
+// Record class -------------------------------------
+class Record
+{
+private:
+    string playerName;  // 玩家名稱
+    string filePath;    // 檔案路徑
+    int maxCards;       // 單局最高手牌數
+    int maxPoints;      // 單局最高點數
+    int maxWinStreak;   // 最高連勝局數
+public:
+    Record(string playerName);  // 建構函式（輸入玩家名稱）
+    void updateRecord(int cardCnt, int pointCnt, int winStreak);  // 更新玩家檔案
+    void print() const;  // 列印玩家檔案
 };
