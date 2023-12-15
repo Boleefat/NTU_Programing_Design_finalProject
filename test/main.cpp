3cdcde3e0aae9cff852b6bfb546f00fc004c73b9
Kaiwei Chen <127728415+kaiwei1016@users.noreply.github.com>
2023-12-16 06:42:13 +0800

    改成多回合制

--- test/main.cpp
+++ test/main.cpp
@@ -5,29 +5,49 @@
 #include <algorithm>
 #include <ctime>
 #include <fstream>
-// #include "English_BlackJack.h"////////////////////注意這邊要換一夏
 #include "BlackJack.h"
 using namespace std;
+
 
 int main()
 {
     // initial setting
     Game game; // 創建遊戲物件
     CardDeck deck;
-    game.printStage("Welcome to Black Jack!");
+    game.printStage("Ｗelcome to Black Jack!");
     game.addPlayers();            // 加入玩家
     game.showPlayersNameAndChr(); // 確認玩家資訊
     game.enterYtoContinue();
-    game.initialDeal(); // 進行初始發牌
-    Player *enemy = new Enemy("Banker");
-    game.addPlayer(enemy); // 加入敵人（莊家）
 
-    game.drawRound(); // 進行21點
-    bool whoWins[2] = {0, 0};
-    game.result(whoWins); // 判斷勝負
-
-    game.itemRound(whoWins);      // Bonus, 道具卡回合
-    game.printStage("GAME ENDS"); // 遊戲結束
-
+    Player* enemy = new Enemy("Banker");
+    game.addPlayer(enemy);  // 加入敵人（莊家）
+    
+    bool play = true;
+    while(play)
+    {
+        game.initialDeal();  // 進行初始發牌
+        game.drawRound();    // 進行21點
+        
+        
+        bool whoWins[2] = {0,0};  // 用來記錄勝負
+        
+        while(play)
+        {
+            game.result(whoWins);     // 判斷勝負
+            game.itemRound(whoWins, play);  // Bonus道具卡回合
+        }
+        
+        // 更新玩家戰績
+        game.printStage("Player's Records");
+        cout << endl;
+        game.getPlayerAtIndex(0)->getRecord()->updateRecord(whoWins[0], game.getPlayerAtIndex(0));
+        game.getPlayerAtIndex(1)->getRecord()->updateRecord(whoWins[1], game.getPlayerAtIndex(1));
+        
+        game.printStage("Black Jack ENDS");
+        play = game.playAgain();  // 是否要再玩一次
+    }
+    
+    game.printStage("Program ENDS");
+    
     return 0;
 }
